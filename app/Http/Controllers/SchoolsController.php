1dd9132f45abf007d5dcc34299575881340d5e81
Evaldas88 <e.skackauskas@gmail.com>
2022-12-08 13:17:55 +0200

    added school controller

--- app/Http/Controllers/SchoolsController.php
+++ app/Http/Controllers/SchoolsController.php
@@ -4,82 +4,134 @@ namespace App\Http\Controllers;
 
 use App\Models\Schools;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SchoolsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
-    }
+        $schools = Schools::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+        if ($schools)
+            return response()->json([
+                'success' => true,
+                'message' => $schools
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Cant find school list'
+            ], 500);
+    }
+    public function show($id, Request $request)
     {
-        //
+
+        $school = Schools::where('id', $id);
+
+        if ($school->get())
+            return response()->json([
+                'success' => true,
+                'message' => $school->get()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Cant find school'
+            ], 500);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        //Authentification
+        if (auth()->user()->role != 0)
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $this->validate($request, [
+            'name' => 'required',
+            'code' => 'required',
+            'city' => 'required',
+            'address' => 'required'
+        ]);
+        $school = new Schools();
+        $school->name = $request->name;
+        $school->code = $request->code;
+        $school->city = $request->city;
+        $school->address = $request->address;
+
+        
+        if ($school->save())
+            return response()->json([
+                'success' => true,
+                'message' => $school->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Cant save  '
+            ], 500);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Schools  $schools
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Schools $schools)
+    public function update($id, Request $request)
     {
-        //
+        //Authentification
+        if (auth()->user()->role != 0)
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $this->validate($request, [
+            'name' => 'required',
+            'code' => 'required',
+            'address' => 'required'
+        ]);
+
+        $school = Schools::find($id);
+        $data = [];
+
+        $data['name'] = $request->name;
+        $data['code'] = $request->code;
+        $data['city'] = $request->city;
+        $data['address'] = $request->address;
+
+
+        if ($school->update($data))
+            return response()->json([
+                'success' => true,
+                'message' => $school->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Cant update  '
+            ], 500);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Schools  $schools
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Schools $schools)
+    public function destroy($id)
     {
-        //
+        //Authentification
+        if (auth()->user()->role != 0)
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $school = Schools::where('id', $id);
+
+        if ($school->delete())
+            return response()->json([
+                'success' => true,
+                'message' => 'School deleted '
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Cant delete school'
+            ], 500);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Schools  $schools
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Schools $schools)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Schools  $schools
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Schools $schools)
-    {
-        //
-    }
 }
