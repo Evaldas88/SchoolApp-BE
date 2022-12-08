dd716afe2e385d519efba9e239c0ddc2a3d5cb55
Evaldas88 <e.skackauskas@gmail.com>
2022-12-08 12:22:54 +0200

    created tables and migrations

---
+++ database/migrations/2022_12_08_102145_create_applications_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('approved')->default(0);
+            $table->integer('student_id')->default(0);
+            $table->integer('class')->default(0);
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->date('student_bd')->nullable();
+            $table->unsignedBigInteger('school_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('school_id')->references('id')->on('schools');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+};
