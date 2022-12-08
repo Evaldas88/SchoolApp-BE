b411177acaa86f9ce29c8ee960106223504a8a6d
Evaldas88 <e.skackauskas@gmail.com>
2022-12-08 13:28:18 +0200

    added api routers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\SchoolsController;
+use App\Http\Controllers\ApplicationsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportAuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+Route::post('logout', [PassportAuthController::class, 'logout'])->middleware('auth:api');
+Route::get('authcheck', [PassportAuthController::class, 'index'])->middleware('auth:api');
+
+Route::get('schools', [SchoolsController::class, 'index']);
+Route::get('schools/{id}', [SchoolsController::class, 'show']);
+Route::post('schools', [SchoolsController::class, 'store'])->middleware('auth:api');
+Route::post('schools/{id}', [SchoolsController::class, 'update'])->middleware('auth:api');
+Route::delete('schools/{id}', [SchoolsController::class, 'destroy'])->middleware('auth:api');
+
+Route::get('applications', [ApplicationsController::class, 'index'])->middleware('auth:api');
+Route::get('applications/all', [ApplicationsController::class, 'all'])->middleware('auth:api');
+Route::get('applications/{id}', [ApplicationsController::class, 'status'])->middleware('auth:api');
+Route::post('applications', [ApplicationsController::class, 'store'])->middleware('auth:api');
+Route::post('student/register', [ApplicationsController::class, 'store'])->middleware('auth:api');
+Route::delete('applications/{id}', [ApplicationsController::class, 'destroy'])->middleware('auth:api');
